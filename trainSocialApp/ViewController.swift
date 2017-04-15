16299ca4f223078161f0adb29c8c56e43f5d8a32
yyokii <yyokii.h@gmil.com>
2017-04-15 19:19:47 +0900

    Initial Commit

---
+++ trainSocialApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trainSocialApp
+//
+//  Created by 東原与生 on 2017/04/15.
+//  Copyright © 2017年 yoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
